d0527105348694ce3b181729543dc16efeecbfe9
agmui <anthonynmui@gmail.com>
2023-07-19 19:21:46 -0400

    added pico to pico demo

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # rm_pico_dev
+
+To read printout:  
+`screen /dev/ttyACM0 9600`
+
+### to upload:  
+first find what port: `sudo dmesg | tail`
+then to upload:  
+`sudo mount /dev/sda1 /media/agmui/ ; sudo cp rm_pico_dev.uf2 /media/agmui/ ; sudo umount /media/agmui/`
---
+++ pico_2_pico_demo/pico_transmit.cpp
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+extern "C"
+{
+#include "can2040.h"
+}
+#include "RP2040.h"
+
+typedef struct can2040_msg CANMsg;
+static struct can2040 cbus;
+
+static void
+can2040_cb(struct can2040 *cd, uint32_t notify, struct can2040_msg *msg)
+{
+}
+
+static void
+PIOx_IRQHandler(void)
+{
+    can2040_pio_irq_handler(&cbus);
+}
+
+void canbus_setup(void)
+{
+    uint32_t pio_num = 0;
+    uint32_t sys_clock = 125000000, bitrate = 500000;
+    uint32_t gpio_rx = 4, gpio_tx = 5;
+
+    // Setup canbus
+    can2040_setup(&cbus, pio_num);
+    can2040_callback_config(&cbus, can2040_cb);
+
+    // Enable irqs
+    irq_set_exclusive_handler(PIO0_IRQ_0_IRQn, PIOx_IRQHandler);
+    NVIC_SetPriority(PIO0_IRQ_0_IRQn, 1);
+    NVIC_EnableIRQ(PIO0_IRQ_0_IRQn);
+
+    // Start canbus
+    can2040_start(&cbus, sys_clock, bitrate, gpio_rx, gpio_tx);
+}
+
+const char *pilk = "pilk";
+int main()
+{
+    stdio_init_all();
+    canbus_setup();
+
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    while (true)
+    {
+        CANMsg msg = {0};
+        msg.dlc = 8;
+        msg.id = 0x200;
+        // clear buffer
+        msg.data[0] = 0x00;
+        msg.data[1] = 0x00;
+        msg.data[2] = 0x00;
+        msg.data[3] = 0x00;
+        msg.data[4] = 0x00;
+        msg.data[5] = 0x00;
+        msg.data[6] = 0x00;
+        msg.data[7] = 0x00;
+
+        for (int i = 0; i < 5; i++)
+        {
+            msg.data[i] = (int)*(pilk + i);
+        }
+
+        int res = can2040_transmit(&cbus, &msg);
+        printf("Sending! PILK returned: %d\n", res);
+
+        // printf("LED ON!\n");
+        gpio_put(LED_PIN, 1);
+        sleep_ms(250);
+        // printf("LED OFF!\n");
+        gpio_put(LED_PIN, 0);
+        sleep_ms(250);
+    }
+
+    return 0;
+}
