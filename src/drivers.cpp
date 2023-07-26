ebcbfd2dbb717ab019d667886c473b3084abcab2
agmui <anthonynmui@gmail.com>
2023-07-26 16:42:53 -0400

    added submoduels to build

--- src/drivers.cpp
+++ src/drivers.cpp
@@ -1,6 +1,16 @@
+#include <stdio.h>
 #include <iostream>
+
 #include "pico/stdlib.h"
+
 #include "drivers.hpp"
+
+// #include "can2040.h"
+
+// #include "f_util.h"
+// #include "ff.h"
+// #include "pico/stdlib.h"
+// #include "rtc.h"
 
 namespace driver
 {
@@ -18,9 +28,11 @@ namespace driver
             // Turn On LED
             gpio_put(25, 1); // Set pin 25 to high
             printf("LED switched on!\n");
+            sleep_ms(250);
             // Turn Off LED
             gpio_put(25, 0); // Set pin 25 to high.
             printf("LED switched off!\n");
+            sleep_ms(250);
         }
     }
 }
