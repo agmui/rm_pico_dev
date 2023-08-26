2ed0112d3895ad479b98f0ef8e1f8a42403d8486
agmui <anthonynmui@gmail.com>
2023-08-26 19:55:15 -0400

    updated wiki

--- test/testlib.cpp
+++ test/testlib.cpp
@@ -1,12 +1,3 @@
-#define CATCH_CONFIG_MAIN
-#define CATCH_CONFIG_NO_POSIX_SIGNALS
-#include <catch2/catch.hpp>
-// #include <drivers.h>
+#include <drivers.h>
+#include <gtest/gtest.h>
 
-TEST_CASE( "Quick check", "[main]" ) {
-    // std::vector<double> values {1, 2., 3.};
-    // auto [mean, moment] = accumulate_vector(values);
-
-    // REQUIRE( mean == 2.0 );
-    // REQUIRE( moment == Approx(4.666666) );
-}
