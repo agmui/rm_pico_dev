f38d8366bc355381c5b2c1d0708c51e87afe2530
agmui <anthonynmui@gmail.com>
2023-10-26 03:22:29 -0400

    added cmocka unit test lib

--- test/testlib.cpp
+++ test/testlib.cpp
@@ -1,3 +1,8 @@
-#include <drivers.h>
-#include <gtest/gtest.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <stdint.h>
+#include <cmocka.h>
+#include <stdio.h>
 
+#include <drivers.h>
